e5e2e4bd8097dbbe6449a80d7cf88aad70c87c1f
Denny <40923967+ninicdennis@users.noreply.github.com>
2022-07-27 19:51:00 -0600

    added loaders when logging in to show progress

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,13 +1,15 @@
-import { Card, Center, PasswordInput, Title, TextInput, Button, Grid, Group } from '@mantine/core';
+import { Card, Center, PasswordInput, Title, TextInput, Button, Grid, Group, LoadingOverlay } from '@mantine/core';
 import Link from 'next/link';
 import { useForm } from '@mantine/form';
 import { useUserState } from '../../stores/Authentication';
 import Router from 'next/router';
 import { showNotification } from '@mantine/notifications';
 import { InitialState } from '../../stores/types';
+import { useState } from 'react';
 
 const LoginPage = ({ user, session }: InitialState) => {
 	const [, actions] = useUserState();
+	const [visible, setVisible] = useState(false);
 
 	if (session) {
 		Router.push('/home');
@@ -26,16 +28,18 @@ const LoginPage = ({ user, session }: InitialState) => {
 	});
 
 	const loginUser = () => {
+		setVisible(true);
 		actions
 			.login(form.values)
-			.then(() =>
+			.then(() => {
 				showNotification({
 					title: 'Logged in!',
 					message: 'Welcome back!',
 					autoClose: 3000,
 					color: 'green',
-				})
-			)
+				});
+				setVisible(false);
+			})
 			.catch((err) => {
 				showNotification({
 					title: 'Error!',
@@ -43,39 +47,43 @@ const LoginPage = ({ user, session }: InitialState) => {
 					autoClose: 3000,
 					color: 'red',
 				});
+				setVisible(false);
 			});
 	};
 
 	return (
 		<Center>
 			<Card shadow='sm' p='lg'>
-				<Title>Login</Title>
-				<form onSubmit={form.onSubmit(loginUser)}>
-					<Grid justify='center' align='center'>
-						<Grid.Col>
-							<TextInput label='Email' required {...form.getInputProps('email')} />
-						</Grid.Col>
-						<Grid.Col>
-							<PasswordInput
-								placeholder='Password'
-								label='Password'
-								required
-								{...form.getInputProps('password')}
-							/>
-						</Grid.Col>
-						<Grid.Col>
-							<Group>
-								<Button type='submit'>Login</Button>
-								<Button variant='outline'>Forgot Password</Button>
-								<Link href='/signup' passHref>
-									<Button component='a' variant='outline'>
-										Register
-									</Button>
-								</Link>
-							</Group>
-						</Grid.Col>
-					</Grid>
-				</form>
+				<Title sx={{ marginBottom: 8 }}>Login</Title>
+				<div style={{ width: 400, position: 'relative' }}>
+					<LoadingOverlay visible={visible} overlayBlur={2} />
+					<form onSubmit={form.onSubmit(loginUser)}>
+						<Grid justify='center' align='center'>
+							<Grid.Col>
+								<TextInput label='Email' required {...form.getInputProps('email')} />
+							</Grid.Col>
+							<Grid.Col>
+								<PasswordInput
+									placeholder='Password'
+									label='Password'
+									required
+									{...form.getInputProps('password')}
+								/>
+							</Grid.Col>
+							<Grid.Col>
+								<Group>
+									<Button type='submit'>Login</Button>
+									<Button variant='outline'>Forgot Password</Button>
+									<Link href='/signup' passHref>
+										<Button component='a' variant='outline'>
+											Register
+										</Button>
+									</Link>
+								</Group>
+							</Grid.Col>
+						</Grid>
+					</form>
+				</div>
 			</Card>
 		</Center>
 	);
--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,42 +1,61 @@
-import { Card, Center, PasswordInput, Title, TextInput, Button, Grid, Group, Loader } from '@mantine/core';
+import {
+	Card,
+	Center,
+	PasswordInput,
+	Title,
+	TextInput,
+	Button,
+	Grid,
+	Group,
+	Loader,
+	LoadingOverlay,
+} from '@mantine/core';
 import Link from 'next/link';
 import { useForm } from '@mantine/form';
 import Router from 'next/router';
 import { useUserState } from '../../stores/Authentication';
 import { showNotification } from '@mantine/notifications';
 import { InitialState } from '../../stores/types';
+import { useState } from 'react';
 
 const RegisterPage = ({ user, session }: InitialState) => {
 	const [, actions] = useUserState();
+	const [visible, setVisible] = useState(false);
+
 	if (session) {
 		Router.push('/home');
 	}
 
-	const form = useForm({
+	const form: any = useForm({
 		initialValues: {
 			email: '',
 			password: '',
 			username: '',
+			secondaryPassword: '',
 		},
 
 		validate: {
 			email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
 			username: (value) => (value.length > 5 ? null : 'Invalid username'),
 			password: (value) => (value.length > 6 ? null : 'Invalid password'),
+			secondaryPassword: (value) => (form.values.password === value ? null : 'Passwords do not match!'),
 		},
 	});
 
 	const register = () => {
+		console.log('test');
+		setVisible(true);
 		actions
 			.register(form.values)
-			.then(() =>
+			.then(() => {
 				showNotification({
 					title: 'Logged in!',
 					message: 'Welcome!',
 					autoClose: 3000,
 					color: 'green',
-				})
-			)
+				});
+				setVisible(false);
+			})
 			.catch((err) => {
 				showNotification({
 					title: 'Error!',
@@ -44,41 +63,53 @@ const RegisterPage = ({ user, session }: InitialState) => {
 					autoClose: 3000,
 					color: 'red',
 				});
+				setVisible(false);
 			});
 	};
 
 	return (
 		<Center>
 			<Card shadow='sm' p='lg'>
-				<Title>Register</Title>
-				<form onSubmit={form.onSubmit(register)}>
-					<Grid justify='center' align='center'>
-						<Grid.Col>
-							<TextInput label='Username' required {...form.getInputProps('username')} />
-						</Grid.Col>
-						<Grid.Col>
-							<TextInput label='Email' required {...form.getInputProps('email')} />
-						</Grid.Col>
-						<Grid.Col>
-							<PasswordInput
-								placeholder='Password'
-								label='Password'
-								required
-								{...form.getInputProps('password')}
-							/>
-						</Grid.Col>
-						<Grid.Col>
-							<Group>
-								<Button type='submit'>Register</Button>
-								<Link href='/login' passHref>
-									<Button component='a' variant='outline'>
-										Login instead
-									</Button>
-								</Link>
-							</Group>
-						</Grid.Col>
-					</Grid>
-				</form>
+				<Title sx={{ marginBottom: 8 }}>Register</Title>
+				<div style={{ width: 400, position: 'relative' }}>
+					<LoadingOverlay visible={visible} overlayBlur={2} />
+					<form onSubmit={form.onSubmit(register)}>
+						<Grid justify='center' align='center'>
+							<Grid.Col>
+								<TextInput label='Username' required {...form.getInputProps('username')} />
+							</Grid.Col>
+							<Grid.Col>
+								<TextInput label='Email' required {...form.getInputProps('email')} />
+							</Grid.Col>
+							<Grid.Col>
+								<PasswordInput
+									placeholder='Password'
+									label='Password'
+									required
+									{...form.getInputProps('password')}
+								/>
+							</Grid.Col>
+							<Grid.Col>
+								<PasswordInput
+									placeholder='Password'
+									label='Confirm Password'
+									required
+									{...form.getInputProps('secondaryPassword')}
+								/>
+							</Grid.Col>
+							<Grid.Col>
+								<Group>
+									<Button type='submit'>Register</Button>
+									<Link href='/login' passHref>
+										<Button component='a' variant='outline'>
+											Login instead
+										</Button>
+									</Link>
+								</Group>
+							</Grid.Col>
+						</Grid>
+					</form>
+				</div>
 			</Card>
 		</Center>
 	);
